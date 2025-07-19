2007389d7873d3afab4fd33c98787f185cb57c42
Priyu-0024 <priyankaarathod11@gmail.com>
2025-07-19 17:58:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Navbar from './components/Navbar';
+//import About from './components/About';
+import TextForm from './components/TextForms';
+function App() { 
+return(
+  <>
+   {/*<Navbar title="Navbarr" homeText="Home page"/>*/}
+    <Navbar/>
+   <div className="my-3">
+  <TextForm heading="Enter the text analyze"/>
+ {/* <About/> */}
+</div>
+</>
+  );
+}
+export default App;
