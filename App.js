46fda37ec0597a87d8274cb58558580b7d8c94e7
Priyu-0024 <priyankaarathod11@gmail.com>
2025-07-19 17:59:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,16 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Navbar from './components/Navbar';
-//import About from './components/About';
-import TextForm from './components/TextForms';
+import About from './components/About';
+//import TextForm from './components/TextForms';
 function App() { 
 return(
   <>
    {/*<Navbar title="Navbarr" homeText="Home page"/>*/}
     <Navbar/>
    <div className="my-3">
-  <TextForm heading="Enter the text analyze"/>
- {/* <About/> */}
+  {/* <TextForm heading="Enter the text analyze"/>*/}
+  <About/> 
 </div>
 </>
   );
